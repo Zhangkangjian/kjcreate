807cf3b8ae5d9fb9c7f9b5537c91e897fbf6c89e
Zhangkangjian <346050163@qq.com>
2018-04-17 09:56:57 +0800

    remove

--- src/store/dataEchart.js
+++
@@ -1,9 +0,0 @@
-import * as types from './types.js'
-import api from '../api'
-import OSS from 'ali-oss'
-import co from 'co'
-var index = 0,mo=0
-
-export default {
-
-}
--- src/store/types.js
+++
@@ -1,6 +0,0 @@
-//export const LOADING_FLAG = 'LOADING_FLAG'
-export const SAMPLELISTVO = 'SAMPLELISTVO'//sampleListVo
-export const MODELSTATE = 'MODELSTATE'//modelState
-export const ADDPAGES = 'ADDPAGES'//ADDPAGES
-export const REMOVELIST = 'REMOVELIST'//ADDPAGES
-export const SAMPLEDETAILl = 'SAMPLEDETAILl'
